5e0e7cf076d14383592d79d209923480486a9bb7
Andy Murph <murph@email.com>
2018-09-12 08:34:14 -0400

    still working on "Navigation Drawer" look at TODO 9/12

--- app/src/main/java/com/bkk/android/redsubmarine/MainActivity.java
+++ app/src/main/java/com/bkk/android/redsubmarine/MainActivity.java
@@ -1,6 +1,8 @@
 package com.bkk.android.redsubmarine;
 
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -53,14 +56,23 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager mLayoutManager;
 
     private Menu sortMenu;
+    private DrawerLayout mDrawerLayout;
 
-    // TODO: 9/11 add a Navigation Drawer
-//    https://developer.android.com/training/implementing-navigation/nav-drawer
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        /* Navigation Drawer*/
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // TODO: 9/12 add a Navigation Drawer
+        // https://developer.android.com/training/implementing-navigation/nav-drawer
 
 
         mRecyclerView = findViewById(R.id.rv_redditPosts);
