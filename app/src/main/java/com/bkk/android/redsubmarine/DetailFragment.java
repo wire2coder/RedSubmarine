abaa520ff89c329e843ee0db0fff7d2ec0af89f1
Andy Murph <murph@email.com>
2018-09-22 10:14:21 -0400

    look at TODO 9/22

--- app/src/main/java/com/bkk/android/redsubmarine/DetailFragment.java
+++ app/src/main/java/com/bkk/android/redsubmarine/DetailFragment.java
@@ -12,11 +12,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.bkk.android.redsubmarine.model.RedditPost;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class DetailFragment extends Fragment {
 
@@ -55,11 +67,60 @@ public class DetailFragment extends Fragment {
         tv_post_title.setText(redditPost1.getTitle());
 
 
+
         // TODO: 9/22 fetch and add comments to a RecyclerView, need to make a new Adapter
+        // https://stackoverflow.com/questions/21579918/retrieving-comments-from-reddits-api
+        // $.getJSON("http://www.reddit.com/r/" + sub + "/comments/" + id + ".json?", function (data)
+        // https://www.reddit.com//r/funny/comments/9hwreb/a_shark_hanging_upside_down_looks_like_someone/.json
+        // TODO: 9/22 make a network request to get comments
+
+         // volleyRequest(); << this doesn't work
 
 
         return rootView;
     } // onCreateView()
 
+
+    // helper
+    public void volleyRequest() {
+
+        RequestQueue queue1 = Volley.newRequestQueue( getActivity().getBaseContext() );
+        String COMMENTS_URL1 = "https://www.reddit.com//r/funny/comments/9hwreb/a_shark_hanging_upside_down_looks_like_someone/.json";
+
+
+        JsonObjectRequest request1 = new JsonObjectRequest(Request.Method.GET
+                , COMMENTS_URL1
+                , (String) null
+                , new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("fragment_detail", response.toString());
+
+//                mAdapter.clearAdapter();
+
+                // parse JSON data
+                // data(JSON object) >> children(JSON array) >> data(JSON object)
+
+
+            } // onResponse
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+//                VolleyLog.d(LOG_TAG, "fragment Error: " + error.getMessage());
+
+                Toast.makeText(getActivity().getBaseContext()
+                        , "VolleyError in Fragment",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+
+        // Add the request to the RequestQueue, what is this "Queue" ?
+        queue1.add(request1);
+
+    } // volleyRequest
 
 } // class DetailFragment
